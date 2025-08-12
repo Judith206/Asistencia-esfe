cd560e8a610832f3ddc1bb66f7d3dabc78d05083
cp25001@esfe.agape.edu.sv <cp25001@esfe.agape.edu.sv>
2025-08-12 13:05:19 -0600

    finalizacion de crud grupo

--- src/main/java/com/esfe/Asistencia/controladores/GrupoController.java
+++ src/main/java/com/esfe/Asistencia/controladores/GrupoController.java
@@ -11,14 +11,19 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.esfe.Asistencia.Modelos.Grupo;
 import com.esfe.Asistencia.Servicios.Interfaces.IGrupoService;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
 
 @Controller
 @RequestMapping("/grupos")// Define la direccion  URL de  el controller
@@ -31,9 +36,10 @@ public class GrupoController {
     public String index(Model model,
                         @RequestParam("page") Optional<Integer> page,
                         @RequestParam("size") Optional<Integer> size){
-        int currentPage = page.orElse(1) - 1;
+        int currentPage = page.orElse(1);
         int pageSize = size.orElse(5);
-        Pageable pageable = PageRequest.of(currentPage, pageSize);
+        
+        Pageable pageable = PageRequest.of(currentPage - 1, pageSize);
         
         Page<Grupo> grupos = grupoService.buscarTodos(pageable);
         model.addAttribute("grupos", grupos);
@@ -48,6 +54,71 @@ public class GrupoController {
         return "grupo/index";
 
     }
+    // Crear 
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("grupo", new Grupo());
+        model.addAttribute("action", "create");
+        return "grupo/mant";
+    }
+
+    //editar
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model){
+       Grupo grupo = grupoService.buscarPorId(id).orElseThrow();
+       model.addAttribute("grupo", grupo);
+       model.addAttribute("action", "edit");
+       return "grupo/mant";
+    }
+    //ver (solo lectura)
+    @GetMapping("/view/{id}")
+    public String view(@PathVariable Integer id, Model model){
+        Grupo grupo = grupoService.buscarPorId(id).orElseThrow();
+        model.addAttribute("grupo", grupo);
+        model.addAttribute("action", "view");
+        return "grupo/mant";
+    
+    }
+    //Eliminar
+    @GetMapping("/delete/{id}")
+    public String deleteConfirm(@PathVariable Integer id, Model model) {
+        Grupo grupo = grupoService.buscarPorId(id).orElseThrow();
+        model.addAttribute("grupo", grupo);
+        model.addAttribute("action", "delete");
+        return "grupo/mant";
+    }
+
+    //procesar
+    @PostMapping("/create")
+    public String saveNuevo(@ModelAttribute Grupo grupo, BindingResult result, RedirectAttributes redirect, Model model) {
+        if(result.hasErrors()){
+            model.addAttribute("action", "create");
+            return "grupo/mant";
+        }
+        grupoService.crearOeditar(grupo);
+            redirect.addFlashAttribute("msg", "Grupo creado correctamente");
+            return "redirect:/grupos";
+    }
+    @PostMapping("/edit")
+    public String saveEditado(@ModelAttribute Grupo grupo, BindingResult result,RedirectAttributes redirect, Model model){
+        if(result.hasErrors()){
+            model.addAttribute("action", "edit");
+            return "grupo/mant";
+        }
+        grupoService.crearOeditar(grupo);
+        redirect.addFlashAttribute("msg", "Grupo actualizado correctamente");
+        return "redirect:/grupos";
+    }
+    @PostMapping("/delete")
+    public String deletegrupo (@ModelAttribute Grupo grupo, RedirectAttributes redirect){
+        grupoService.eliminarPorId(grupo.getId());
+        redirect.addFlashAttribute("msg", "Grupo eliminado correctamente");
+        return "redirect:/grupos";
+
+    }
+
     
     
+
 }
